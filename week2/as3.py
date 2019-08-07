b59408a15a4a0413d398c3c097f471712b9ef503
nanda <33324450+Bigblue00@users.noreply.github.com>
2019-08-07 12:16:30 +0530

    Update as3.py

--- week2/as3.py
+++ week2/as3.py
@@ -19,7 +19,8 @@ Output:
 
 
 n = int(input())
-a=list(map(int,input().split()));
-for i in range(n):
-  print(a[i]+a[-i-1],end=" ");
+a=list(map(int,input().split()))
+for i in range(n-1):
+  print(a[i]+a[-i-1],end=" ")
+  print(a[0]+a[-1],end="")
 
