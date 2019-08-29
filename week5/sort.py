08bca8c619034b35dd19fcfa716b4bea4482881b
nanda <33324450+Bigblue00@users.noreply.github.com>
2019-08-29 11:47:13 +0530

    Update sort.py

--- week5/sort.py
+++ week5/sort.py
@@ -1,13 +1,7 @@
-def sorting(a):
-	n=len(a)
-	count=0
-	for i in range(n):
-		for j in range(0,n-i-1):
-			if(a[j]>a[j+1]):
-				a[j],a[j+1]=a[j+1],a[j]
-				count+=1
-	print(count)
-
-
 a=list(map(int,input().split()))
-sorting(a)
+x1=sorted(a)
+c=0
+for i in range(len(a)):
+    if a[i]==x1[c]:
+        c=c+1
+print(len(a)-c,end="")
